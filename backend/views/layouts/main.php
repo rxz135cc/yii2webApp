5c317d95fd4779d2d19c8ccef612961f6a80ddde
rxz135cc <shaffiq666@gmail.com>
2015-11-10 02:46:53 +0800

    testing the architecture 4

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -54,8 +54,6 @@ AppAsset::register($this);
 
             <div class="jumbotron">
                 <h1>Student Attendance System</h1>
-                <p>Practice makes perfect</p>  
-                <p>Testing Debian Server</p>        
                 <a href="#" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-search"></span> Search</a>
             </div>
 
