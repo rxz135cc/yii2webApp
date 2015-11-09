67c7fa5a0fe7e64c3b69d64a209365177a9a2737
rxz135cc <shaffiq666@gmail.com>
2015-11-10 01:11:14 +0800

    update acceptance test

--- backend/tests/acceptance/SigninCept.php
+++ backend/tests/acceptance/SigninCept.php
@@ -5,5 +5,5 @@ $I->amOnPage('/web/index.php?r=site%2Flogin');
 $I->fillField('Username','shafiq');
 $I->fillField('Password','Takjugak6799');
 $I->click('Login');
-$I->see('Student Attendance System');
+$I->see('Student Attendance');
 ?>
