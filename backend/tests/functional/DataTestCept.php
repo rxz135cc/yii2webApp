a795c125c6eb6b922116f971f6f497e125b76bfa
imaprog <shafiqaziz06@gmail.com>
2016-04-12 16:38:51 +0800

    add more test for DB codeception

--- backend/tests/functional/DataTestCept.php
+++ backend/tests/functional/DataTestCept.php
@@ -1,4 +1,6 @@
 <?php 
 $I = new FunctionalTester($scenario);
 $I->seeInDatabase('student', array('student_name' => 'Ali Baba', 'student_email' => 'siap_kau@yahoo.com'));
+$I->haveInDatabase('student', array('student_matrix' => '2013708301', 'student_name' => 'Mohamad Shafiq Bin Aziz', 'program_id' => '1', 'campus_id' => '1', 'student_phone' => '01133396872', 'student_email' => 'shafiqaziz06@gmail.com'));
+$I->seeInDatabase('student', array('student_matrix' => '2013708301', 'student_email' => 'shafiqaziz06@gmail.com'));
 ?>
