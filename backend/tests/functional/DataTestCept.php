5e9e383933e16752e0ddd2b53b0512407002c709
imaprog <shafiqaziz06@gmail.com>
2016-04-12 20:23:26 +0800

    add rules to DataTest

--- backend/tests/functional/DataTestCept.php
+++ backend/tests/functional/DataTestCept.php
@@ -3,4 +3,5 @@ $I = new FunctionalTester($scenario);
 $I->seeInDatabase('student', array('student_name' => 'Ali Baba', 'student_email' => 'siap_kau@yahoo.com'));
 $I->haveInDatabase('student', array('student_matrix' => '2013708301', 'student_name' => 'Mohamad Shafiq Bin Aziz', 'program_id' => '1', 'campus_id' => '1', 'student_phone' => '01133396872', 'student_email' => 'shafiqaziz06@gmail.com'));
 $I->seeInDatabase('student', array('student_matrix' => '2013708301', 'student_email' => 'shafiqaziz06@gmail.com'));
+$I->seeNumRecords(1, 'student', ['student_matrix' => '2013708301'])
 ?>
