e2da1d8927ef4ac95e2c83d94c67d2de68d5db74
imaprog <shafiqaziz06@gmail.com>
2016-04-15 16:45:28 +0800

    update codeception acceptance test

--- backend/tests/acceptance/formTestCept.php
+++ backend/tests/acceptance/formTestCept.php
@@ -1,20 +1,20 @@
 <?php
-// $I = new AcceptanceTester($scenario);
-// $I->wantTo('Test Form');
-// $I->login('imaprog', 'Takjugak6799');
-// $I->amOnPage('index.php?r=medical-certificate/create');
-// $I->selectOption('MedicalCertificate[student_id]','2010260208');
-// $value = $I->grabValueFrom(['name' => 'MedicalCertificate[student_id]']);
-// $I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
-// $I->sendAjaxGetRequest('index.php?r=student/get-program-email&matrixId='.$value.'');
-// $I->seeResponseIsJson();
-// $I->seeResponseMatchesJsonType([
-//      'student_id' => 'integer',
-//      'student_matrix' => 'string:regex(/[\d]/)',
-//      'student_name' => 'string:regex(/[a-zA-Z]/)',
-//      'program_id' => 'integer',
-//      'campus_id' => 'integer',
-//      'student_phone' => 'string:regex(/[\d]/)',
-//      'student_email' => 'string:regex(/[a-zA-Z\d]+[@a-zA-Z\d]+[.com]/)'
-// ]);
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Test Form');
+$I->login('imaprog', 'Takjugak6799');
+$I->amOnPage('index.php?r=medical-certificate/create');
+$I->selectOption('MedicalCertificate[student_id]','2010260208');
+$value = $I->grabValueFrom(['name' => 'MedicalCertificate[student_id]']);
+$I->haveHttpHeader('Content-Type', 'application/x-www-form-urlencoded');
+$I->sendAjaxGetRequest('index.php?r=student/get-program-email&matrixId='.$value.'');
+$I->seeResponseIsJson();
+$I->seeResponseMatchesJsonType([
+     'student_id' => 'integer',
+     'student_matrix' => 'string:regex(/[\d]/)',
+     'student_name' => 'string:regex(/[a-zA-Z]/)',
+     'program_id' => 'integer',
+     'campus_id' => 'integer',
+     'student_phone' => 'string:regex(/[\d]/)',
+     'student_email' => 'string:regex(/[a-zA-Z\d]+[@a-zA-Z\d]+[.com]/)'
+]);
 ?>
